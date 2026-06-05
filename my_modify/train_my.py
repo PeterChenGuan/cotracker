c565610d34f39be758fcd5905eccb55203614b03
PeterChenGuan <2877891032@qq.com>
2026-06-05 13:24:47 +0800

    test

--- my_modify/train_my.py
+++ my_modify/train_my.py
@@ -4,7 +4,7 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
-
+# 这是随便加的测试
 import os
 import argparse
 
